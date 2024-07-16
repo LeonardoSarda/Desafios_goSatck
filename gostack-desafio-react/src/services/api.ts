8221b29c5e214e6b4d2a04084bfd2ea5deef8996
leonardo sarda <leonardo.sarda@icloud.com>
2024-07-16 10:43:30 -0300

    Feito toda a parte de importação e listagem

--- gostack-desafio-react/src/services/api.ts
+++ gostack-desafio-react/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+// Api de teste uso a porta 3335
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://localhost:3335',
 });
 
 export default api;
